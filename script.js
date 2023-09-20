bc217400a87b3704ff2bce93cb2317981649a8b2
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-09-20 12:19:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
-//your code here!
+const list = document.getElementById('infi-list');
 
+const loadMoreThreshold = 200; // Adjust this threshold as needed
+
+// Function to add two more list items
+function addMoreItems() {
+  for (let i = 0; i < 2; i++) {
+  const moviecard=document.createElement("div");
+        moviecard.classList.add("movie-item")
+    moviecard.innerHTML = list.textContent;
+    list.appendChild(moviecard);
+  }
+}
+
+// Function to check if the user has reached the end of the list
+function isNearBottom() {
+  const scrollTop = list.scrollTop;
+  const scrollHeight = list.scrollHeight;
+  const clientHeight = list.clientHeight;
+
+  return scrollHeight - scrollTop <= clientHeight + loadMoreThreshold;
+}
+
+// Event listener to continuously monitor the scroll position
+list.addEventListener('scroll', () => {
+  if (isNearBottom()) {
+    addMoreItems();
+  }
+});
+
+// Initially load two items to start
+addMoreItems();
